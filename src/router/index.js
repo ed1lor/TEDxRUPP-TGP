1d2a656cb4b2455d06f835a87e86f6c73062901c
unknown <vanndyu@gmail.com>
2020-10-12 07:34:40 +0700

    Added some comments to the code

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/:name',
     //redirect: '/:name',
-    name: 'About',
+    name: 'QRCode Generator',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
